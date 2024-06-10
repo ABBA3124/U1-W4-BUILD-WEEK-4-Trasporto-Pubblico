7e5c68ceedec2c2ff6e8d93d0473f10f33183c04
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-10 11:01:06 +0200

    start

---
+++ src/main/java/org/gestionetrasportopubblico/Application.java
@@ -0,0 +1,13 @@
+package org.gestionetrasportopubblico;
+
+public class Application {
+//    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W4-BUILD-WEEK-4-Trasporto-Pubblico");
+
+    public static void main(String[] args) {
+
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W4-BUILD-WEEK-4-Trasporto-Pubblico");
+
+
+        System.out.println("Start build week");
+    }
+}
