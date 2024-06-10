2a08ebfb86fec17f2079073695c953c1b5ca4714
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-10 12:08:45 +0200

    Utente dao + prova utente

--- src/main/java/org/gestionetrasportopubblico/Application.java
+++ src/main/java/org/gestionetrasportopubblico/Application.java
@@ -1,13 +1,30 @@
 package org.gestionetrasportopubblico;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.gestionetrasportopubblico.dao.UtenteDAO;
+import org.gestionetrasportopubblico.entities.Utente;
+
+import java.time.LocalDate;
+
 public class Application {
-//    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W4-BUILD-WEEK-4-Trasporto-Pubblico");
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W4-BUILD-WEEK-4-Trasporto-Pubblico");
 
     public static void main(String[] args) {
-
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W4-BUILD-WEEK-4-Trasporto-Pubblico");
-
-
         System.out.println("Start build week");
+
+        EntityManager em = emf.createEntityManager();
+        UtenteDAO utenteDAO = new UtenteDAO(em);
+        Utente utente = new Utente();
+        utente.setNome("luca");
+        utente.setCognome("nardi");
+        utente.setNumeroTessera("123456789");
+        utente.setDataScadenzaTessera(LocalDate.now().plusYears(1));
+
+        utenteDAO.create(utente);
+
+        System.out.println(utente + " creato? ");
+
     }
 }
--- src/main/java/org/gestionetrasportopubblico/dao/UtenteDAO.java
+++ src/main/java/org/gestionetrasportopubblico/dao/UtenteDAO.java
@@ -1,4 +1,22 @@
 package org.gestionetrasportopubblico.dao;
 
+import jakarta.persistence.EntityManager;
+import org.gestionetrasportopubblico.entities.Utente;
+
 public class UtenteDAO {
+
+    private final EntityManager em;
+
+    public UtenteDAO(EntityManager em) {
+        this.em = em;
+    }
+
+
+    public void create(Utente utente) {
+        em.getTransaction().begin();
+        em.persist(utente);
+        em.getTransaction().commit();
+        em.close();
+
+    }
 }
