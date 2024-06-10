0a19e5f07e0c95257526c58c97d7fb4733fbfdb1
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-10 11:47:20 +0200

    entita biglietto

--- src/main/java/org/gestionetrasportopubblico/entities/Biglietto.java
+++ src/main/java/org/gestionetrasportopubblico/entities/Biglietto.java
@@ -1,8 +1,68 @@
 package org.gestionetrasportopubblico.entities;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 public class Biglietto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String codiceUnivoco;
+    private LocalDate dataEmissione;
+
+
+    @ManyToOne
+    @JoinColumn(name = "punto_vendita_id")
+    private PuntoVendita puntoVendita;
+
+    public Biglietto() {
+    }
+
+
+    public Biglietto(String codiceUnivoco, LocalDate dataEmissione, PuntoVendita puntoVendita) {
+        this.codiceUnivoco = codiceUnivoco;
+        this.dataEmissione = dataEmissione;
+        this.puntoVendita = puntoVendita;
+    }
+
+    public PuntoVendita getPuntoVendita() {
+        return puntoVendita;
+    }
+
+    public void setPuntoVendita(PuntoVendita puntoVendita) {
+        this.puntoVendita = puntoVendita;
+    }
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public void setDataEmissione(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+    }
+
+    public String getCodiceUnivoco() {
+        return codiceUnivoco;
+    }
+
+    public void setCodiceUnivoco(String codiceUnivoco) {
+        this.codiceUnivoco = codiceUnivoco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Biglietto{" +
+                "id=" + id +
+                ", codiceUnivoco='" + codiceUnivoco + '\'' +
+                ", dataEmissione=" + dataEmissione +
+                ", puntoVendita=" + puntoVendita +
+                '}';
+    }
 }
