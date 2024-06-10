12e633616cc8400e78855d51f601c69cd25af80c
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-10 12:50:56 +0200

    add findById from utente

--- src/main/java/org/gestionetrasportopubblico/Application.java
+++ src/main/java/org/gestionetrasportopubblico/Application.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Persistence;
 import org.gestionetrasportopubblico.dao.UtenteDAO;
 import org.gestionetrasportopubblico.entities.Utente;
 
-import java.time.LocalDate;
+import java.util.UUID;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W4-BUILD-WEEK-4-Trasporto-Pubblico");
@@ -16,15 +16,18 @@ public class Application {
         UtenteDAO utenteDAO = new UtenteDAO();
 
         //crea utente
-        Utente utente = new Utente();
-        utente.setNome("emanuele");
-        utente.setCognome("Ivfdvdfvdfv");
-        utente.setNumeroTessera("46461651");
-        utente.setDataScadenzaTessera(LocalDate.now().plusYears(5));
-        utenteDAO.create(utente);
+//        Utente utente = new Utente();
+//        utente.setNome("emanuele");
+//        utente.setCognome("Ivfdvdfvdfv");
+//        utente.setNumeroTessera("46461651");
+//        utente.setDataScadenzaTessera(LocalDate.now().plusYears(5));
+//        utenteDAO.create(utente);
 
 
-        System.out.println(utente);
+//        System.out.println(utente);
+
+        Utente daTrovare = utenteDAO.findById(UUID.fromString("828028ab-16c2-4657-9e50-1f5ec8c55c5b"));
+        System.out.println("Utente trovato: " + daTrovare.getNome() + " " + daTrovare.getCognome());
 
 
     }
--- src/main/java/org/gestionetrasportopubblico/dao/UtenteDAO.java
+++ src/main/java/org/gestionetrasportopubblico/dao/UtenteDAO.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.gestionetrasportopubblico.entities.Utente;
 
+import java.util.UUID;
+
 public class UtenteDAO {
 
     private EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W4-BUILD-WEEK-4-Trasporto-Pubblico");
@@ -15,5 +17,12 @@ public class UtenteDAO {
         em.persist(utente);
         em.getTransaction().commit();
         em.close();
+    }
+
+    public Utente findById(UUID id) {
+        EntityManager em = emf.createEntityManager();
+        Utente utente = em.find(Utente.class, id);
+        em.close();
+        return utente;
     }
 }
